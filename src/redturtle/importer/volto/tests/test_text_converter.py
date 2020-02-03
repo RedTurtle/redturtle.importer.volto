adff4670a0614163b2c0ff3aa9cdf8a738eb4c67
Andrea Cecchi <andrea.cecchi85@gmail.com>
2020-02-03 14:23:14 +0100

    add tests for extraction tool

--- src/redturtle/importer/volto/tests/test_text_converter.py
+++ src/redturtle/importer/volto/tests/test_text_converter.py
@@ -7,6 +7,17 @@ from redturtle.importer.volto.testing import (
 
 import unittest
 
+SAMPLE_HTML = '''
+<p><em>Nulla</em> sit amet est. Morbi mattis <strong>ullamcorper</strong> velit.</p>
+<p></p>
+<p><a href="http://www.plone.com" data-linktype="external" data-val="http://www.plone.com">this is a link</a></p>
+<ul>
+<li>one</li>
+<li>two</li>
+<li>three</li>
+</ul>
+'''
+
 
 class TestDraftjsConverter(unittest.TestCase):
     """"""
@@ -17,13 +28,93 @@ class TestDraftjsConverter(unittest.TestCase):
         """Custom shared utility setup for tests."""
         self.portal = self.layer['portal']
         self.request = self.layer['request']
-
-    def test_simple_text(self):
-        section = ToVoltoBlocks(
+        self.tool = ToVoltoBlocks(
             transmogrifier=None, name=None, options={}, previous=None
         )
-        result = section.conversion_tool(html='<p>foo</p>')
+
+    def test_simple_text(self):
+        result = self.tool.conversion_tool(html='<p>foo</p>')
         self.assertEqual(len(result), 1)
-        p = result[0]
-        self.assertEqual(p['@type'], 'text')
-        self.assertEqual(p['text']['blocks'][0]['text'], 'foo')
+        p = result[0]['text']['blocks'][0]
+        entityMap = result[0]['text']['entityMap']
+        self.assertEqual(result[0]['@type'], 'text')
+        self.assertEqual(p['entityRanges'], [])
+        self.assertEqual(p['text'], 'foo')
+        self.assertEqual(entityMap, {})
+
+    def test_text_with_inline_styles(self):
+
+        result = self.tool.conversion_tool(
+            html='<p><em>foo</em> bar <strong>baz</strong></p>'
+        )
+        self.assertEqual(len(result), 1)
+        p = result[0]['text']['blocks'][0]
+        entityMap = result[0]['text']['entityMap']
+        self.assertEqual(result[0]['@type'], 'text')
+        self.assertEqual(p['text'], 'foo bar baz')
+        self.assertEqual(p['entityRanges'], [])
+        self.assertEqual(
+            p['inlineStyleRanges'],
+            [
+                {'length': 3, 'offset': 0, 'style': 'ITALIC'},
+                {'length': 3, 'offset': 8, 'style': 'BOLD'},
+            ],
+        )
+        self.assertEqual(entityMap, {})
+
+    def test_text_empty(self):
+        result = self.tool.conversion_tool(html='<p></p>')
+        self.assertEqual(len(result), 1)
+        p = result[0]['text']['blocks'][0]
+        entityMap = result[0]['text']['entityMap']
+        self.assertEqual(result[0]['@type'], 'text')
+        self.assertEqual(p['entityRanges'], [])
+        self.assertEqual(p['text'], '')
+        self.assertEqual(entityMap, {})
+
+    def test_text_with_links(self):
+        result = self.tool.conversion_tool(
+            html='<p><a href="http://www.plone.com" data-linktype="external" data-val="http://www.plone.com">this is a link</a></p>'  # noqa
+        )
+        self.assertEqual(len(result), 1)
+        p = result[0]['text']['blocks'][0]
+        entityMap = result[0]['text']['entityMap']
+        self.assertEqual(result[0]['@type'], 'text')
+        self.assertEqual(p['text'], 'this is a link')
+        self.assertEqual(
+            p['entityRanges'], [{'key': 0, 'length': 14, 'offset': 0}]
+        )
+        self.assertEqual(
+            entityMap,
+            {
+                '0': {
+                    'data': {
+                        'data-linktype': 'external',
+                        'data-val': 'http://www.plone.com',
+                        'url': 'http://www.plone.com',
+                    },
+                    'mutability': 'MUTABLE',
+                    'type': 'LINK',
+                }
+            },
+        )
+
+    def test_text_with_image(self):
+        result = self.tool.conversion_tool(
+            html='<p><img alt="" src="https://www.plone.org/logo.png" class="image-right" data-linktype="image"/></p>'  # noqa
+        )
+        self.assertEqual(len(result), 1)
+        block = result[0]
+        self.assertEqual(block['@type'], 'image')
+        self.assertEqual(block['align'], 'right')
+        self.assertEqual(block['url'], 'https://www.plone.org/logo.png')
+
+        result = self.tool.conversion_tool(
+            html='<p><img alt="" src="https://www.plone.org/logo.png" class="image-inline" data-linktype="image"/></p>'  # noqa
+        )
+        block = result[0]
+        self.assertEqual(block['align'], 'full')
+
+    def test_text_with_multiple_items(self):
+        result = self.tool.conversion_tool(html=SAMPLE_HTML)
+        self.assertEqual(len(result), 4)
